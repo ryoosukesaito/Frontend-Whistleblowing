bd4867791223cfee06c61128af66b8e8720fd619
ryoosukesaito <rshyp196_sai@yahoo.co.jp>
2023-04-06 03:00:32 -0700

    added style color and reflect setting to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "main-color-1": "#074449",
+        "main-color-2": "#02DDB7",
+        "gray-scale-1": "#1B1C1E",
+        "gray-scale-2": "#737373",
+        "gray-scale-3": "#D6D6D6",
+        "gray-scale-4": "#EDEDED",
+        "not-started": "#FFDE85",
+        "in-progress": "#B6E0B6",
+        completed: "#BADFFF",
+      },
+    },
   },
   plugins: [],
 };
