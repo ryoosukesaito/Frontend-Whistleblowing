76c93b03505a925f93bac6cd067941a0667946c6
krrr017 <xfxrx.531@gmail.com>
2023-04-12 22:12:08 +0900

    sidebar and navbar style added

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,37 +1,50 @@
 import React from "react";
 import { sidebarMenu } from "../constants/constants";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import {ChevronDownIcon} from '@heroicons/react/24/solid'
+import {ArrowRightOnRectangleIcon} from '@heroicons/react/24/solid'
 
 const SideBar = () => {
+  const [menubar, setMenubar] = useState(false);
   function handleLogout(e) {
     e.preventDefault();
     window.location.replace("/");
   }
   return (
-    <div className="h-full w-full flex flex-col items-start ">
-      <div href="">
-        <Link to="/api/admin/reports">
-          <button>Reports</button>
+    <div className="h-full w-full flex flex-col px-3 py-4 overflow-y-auto bg-gray-scale-4">
+      <div className="items-start">
+      <div className="my-4">
+        <Link to="/api/admin/reports" className="text-lg py-4">
+          Reports
         </Link>
       </div>
-      <li className="list-none">
+      <div>
         <button
+          className="text-lg flex items-center cursor-pointer py-4"
           type="button"
-          aria-controls="managements-menu"
-          data-collapse-toggle="managements-menu"
+          onClick={() => setMenubar(!menubar)}
         >
           Managements
+          <ChevronDownIcon className="h-6 w-6 ml-3"/>
         </button>
-        <ul id="managements-menu" className="">
+        <ul id="managements-menu" className={"pb-4" + (menubar ? " block" : " hidden")}>
           {sidebarMenu.map((menu, idx) => (
-            <li key={idx}>
+            <li className="py-4" key={idx}>
               <a href={menu.href}>{menu.name}</a>
             </li>
           ))}
         </ul>
-      </li>
-
-      <button onClick={handleLogout}>Sign out</button>
+      </div>
+      </div>
+      
+      <div className="border-t border-t-gray-scale-3  my-4 pt-4 text-lg">
+        <button className="flex items-center text-center cursor-pointer" onClick={handleLogout}>
+          Sign out
+          <ArrowRightOnRectangleIcon className="h-7 w-7 ml-3"/>
+        </button>
+      </div>
+      
     </div>
   );
 };
