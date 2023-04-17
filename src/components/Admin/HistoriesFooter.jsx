979fe070ace70f04cd696991a0903edb946ae8b2
ryoosukesaito <rshyp196_sai@yahoo.co.jp>
2023-04-17 01:59:54 -0700

    removed an unused value of heroicon

--- src/components/Admin/HistoriesFooter.jsx
+++ src/components/Admin/HistoriesFooter.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { DocumentArrowDownIcon } from '@heroicons/react/24/outline'
+import { DocumentArrowDownIcon } from "@heroicons/react/24/outline";
 
 function HistoriesFooter() {
   function handleSubmit(e) {
@@ -12,7 +12,7 @@ function HistoriesFooter() {
         <input type="text" className="h-full w-full mt-2 my-3" />
         <div className="flex flex-row justify-between">
           <button className="cursor-pointe flex text-indigo-700">
-            <DocumentArrowDownIcon className="h-6 w-6 mr-1 " />  
+            <DocumentArrowDownIcon className="h-6 w-6 mr-1 " />
             Upload File
           </button>
           <button className="px-4 py-1 mb-1 rounded cursor-pointe bg-gray-scale-2 hover:bg-gray-100 text-white">
--- src/components/Admin/ReportDetails.jsx
+++ src/components/Admin/ReportDetails.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { reportDataExamples } from "../../data/dataExample";
 import Histories from "./Histories";
-import { PaperClipIcon } from '@heroicons/react/24/outline'
 
 function ReportDetails() {
   return (
@@ -11,27 +10,27 @@ function ReportDetails() {
           <div key={idx}>
             <div className="flex mb-3">
               <table className="w-1/2">
-                  <tbody>
-                    <tr>
-                      <td className="py-2">Report ID</td>
-                      <td className="py-2">: {data._id}</td>
-                    </tr>
-                    <tr>
-                      <td className="py-2">Post Date</td>
-                      <td className="py-2">: {data.createdAt}</td>
-                    </tr>
-                    <tr>
-                      <td className="py-2">Update Date</td>
-                      <td className="py-2">: {data.updatedAt}</td>
-                    </tr>
-                    <tr>
-                      <td className="py-2">Your Name</td>
-                      <td className="py-2">: {data.userName}</td>
-                    </tr>
-                    <tr>
-                      <td className="py-2">Your Department</td>
-                      <td className="py-2"> : {data.userDepartment}</td>
-                    </tr>
+                <tbody>
+                  <tr>
+                    <td className="py-2">Report ID</td>
+                    <td className="py-2">: {data._id}</td>
+                  </tr>
+                  <tr>
+                    <td className="py-2">Post Date</td>
+                    <td className="py-2">: {data.createdAt}</td>
+                  </tr>
+                  <tr>
+                    <td className="py-2">Update Date</td>
+                    <td className="py-2">: {data.updatedAt}</td>
+                  </tr>
+                  <tr>
+                    <td className="py-2">Your Name</td>
+                    <td className="py-2">: {data.userName}</td>
+                  </tr>
+                  <tr>
+                    <td className="py-2">Your Department</td>
+                    <td className="py-2"> : {data.userDepartment}</td>
+                  </tr>
                 </tbody>
               </table>
               <table className="w-1/2">
@@ -46,8 +45,7 @@ function ReportDetails() {
                   </tr>
                   <tr>
                     <td>File</td>
-                    <td className="text-indigo-700">: img-file.jpeg
-                    </td>
+                    <td className="text-indigo-700">: img-file.jpeg</td>
                   </tr>
                   <tr>
                     <td>Your Agent</td>
@@ -56,20 +54,20 @@ function ReportDetails() {
                 </tbody>
               </table>
             </div>
-            
+
             <div className="my-3">
               <span className="text-lg">Description</span>
-              <div className="border p-2">
-                {data.description}
-              </div>
+              <div className="border p-2">{data.description}</div>
             </div>
           </div>
         ))}
       </div>
 
       <div className="border bg-gray-scale-4 p-2">
-          <div className="text-lg border-b-2 border-gray-scale-1">Contact Record</div>
-          <Histories />
+        <div className="text-lg border-b-2 border-gray-scale-1">
+          Contact Record
+        </div>
+        <Histories />
       </div>
     </>
   );
