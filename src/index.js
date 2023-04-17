ddae78c5dcc0f6d7dbd54d5b84ff4678dd6daf4f
ryoosukesaito <rshyp196_sai@yahoo.co.jp>
2023-04-17 03:59:57 -0700

    connected db to AdminReport.jsx

--- src/index.js
+++ src/index.js
@@ -4,18 +4,21 @@ import "./input.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import persistStore from "redux-persist/es/persistStore";
 import store from "./store";
+
+const persistedStore = persistStore(store);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistedStore}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
