c2631ea190065773be8e7866d4234a80f1631b53
ryoosukesaito <rshyp196_sai@yahoo.co.jp>
2023-04-23 05:20:36 -0700

    add feature changing color of report status

--- src/components/Admin/AdminReport.jsx
+++ src/components/Admin/AdminReport.jsx
@@ -10,7 +10,6 @@ import { FunnelIcon } from "@heroicons/react/24/solid";
 
 import ReportFilter from "../Admin/ReportFilter";
 
-
 function AdminReport() {
   const admin = useSelector((state) => state.admin);
   const navigate = useNavigate();
@@ -52,68 +51,72 @@ function AdminReport() {
     <div className="">
       <div className="flex justify-end">
         <button className="flex justify-center items-center w-20 h-6 bg-gray-scale-3 mr-10 cursor-pointer">
-          <FunnelIcon className="h-4 w-4 mr-1.5"/>
+          <FunnelIcon className="h-4 w-4 mr-1.5" />
           Filter
         </button>
       </div>
       <div className="h-full mt-5 flex items-start justify-center">
-      <table className="w-full">
-        <thead>
-          <tr>
-            {reportTableHeaders.map((header, idx) => (
-              <th key={idx} className="border-b-4 border-slate-600">
-                {header}
-              </th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {reports.map((data, idx) => (
-
-            <tr
-              key={data._id}
-              className=" cursor-pointer  hover:bg-gray-scale-3"
-              onClick={handleClick}
-            >
-              <td
-                className="border-b-2 border-slate-700 text-center"
-                data-value={data._id}
-              >
-
-             <div className="my-1 flex justify-center items-center bg-not-started rounded-full">
-                {data.status}
-            </div>
-              </td>
-              <td
-                className="border-b-2 border-slate-700 text-center"
-                data-value={data._id}
-              >
-                {data.subject}
-              </td>
-              <td
-                className="border-b-2 border-slate-700 text-center"
-                data-value={data._id}
-              >
-                {data.adminId}
-              </td>
-              <td
-                className="border-b-2 border-slate-700 text-center"
-                data-value={data._id}
-              >
-                {data.createdAt}
-              </td>
-              <td
-                className="border-b-2 border-slate-700 text-center"
-                data-value={data._id}
-              >
-                {data.updatedAt}
-              </td>
+        <table className="w-full">
+          <thead>
+            <tr>
+              {reportTableHeaders.map((header, idx) => (
+                <th key={idx} className="border-b-4 border-slate-600">
+                  {header}
+                </th>
+              ))}
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {reports.map((data, idx) => (
+              <tr
+                key={data._id}
+                className=" cursor-pointer  hover:bg-gray-scale-3"
+                onClick={handleClick}
+              >
+                <td
+                  className="border-b-2 border-slate-700 text-center"
+                  data-value={data._id}
+                >
+                  {data.status === "Not started" ? (
+                    <div className="my-1 flex justify-center items-center bg-not-started rounded-full">
+                      {data.status}
+                    </div>
+                  ) : (
+                    <div className="my-1 flex justify-center items-center bg-in-progress rounded-full">
+                      {data.status}
+                    </div>
+                  )}
+                </td>
+                <td
+                  className="border-b-2 border-slate-700 text-center"
+                  data-value={data._id}
+                >
+                  {data.subject}
+                </td>
+                <td
+                  className="border-b-2 border-slate-700 text-center"
+                  data-value={data._id}
+                >
+                  {data.adminId}
+                </td>
+                <td
+                  className="border-b-2 border-slate-700 text-center"
+                  data-value={data._id}
+                >
+                  {data.createdAt}
+                </td>
+                <td
+                  className="border-b-2 border-slate-700 text-center"
+                  data-value={data._id}
+                >
+                  {data.updatedAt}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
-        {/* <ReportFilter /> */}
+      {/* <ReportFilter /> */}
     </div>
   );
 }
